c49e4cc77f22029131bd9f71250e4c4d393903e3
Iya Rivvikyn <101973571+iyarivky@users.noreply.github.com>
2022-06-26 18:26:28 +0700

    Update cs-cf.sh

--- Code-Server/cs-cf.sh
+++ Code-Server/cs-cf.sh
@@ -1,5 +1,5 @@
 wget https://github.com/coder/code-server/releases/download/v4.4.0/code-server-4.4.0-linux-amd64.tar.gz
-tar -xvf code-server-4.4.0-linux-amd64.tar.gz
+tar -xf code-server-4.4.0-linux-amd64.tar.gz
 wget https://github.com/cloudflare/cloudflared/releases/download/2022.6.3/cloudflared-linux-amd64
 mv cloudflared-linux-amd64 cloudflared
 chmod +x cloudflared
